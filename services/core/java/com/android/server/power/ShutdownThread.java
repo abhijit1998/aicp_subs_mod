ef30a88851eb5247fb3dfec6ce4cd461a262272a
Ivan Iskandar <ivan@prjkt.io>
2017-09-17 03:01:24 +0200

    Extras: Add dynamic theme shutdown and boot animation support
    Note: Custom boot animation will not work on encrypted device
    
    Change-Id: I3b01953d0a69c033a98c0af49ee986b21652b725
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -369,7 +369,7 @@ public final class ShutdownThread extends Thread {
         attrs.alpha = setRebootDialogAlpha(context);
         pd.getWindow().setDimAmount(setRebootDialogDim(context));
 
-        pd.show();
+        if (!themeShutdownAnimationExists()) pd.show();
         return pd;
     }
 
@@ -471,6 +471,10 @@ public final class ShutdownThread extends Thread {
         {
             String reason = (mReboot ? "1" : "0") + (mReason != null ? mReason : "");
             SystemProperties.set(SHUTDOWN_ACTION_PROPERTY, reason);
+        }
+
+        if (themeShutdownAnimationExists()) {
+            startShutdownAnimation();
         }
 
         /*
@@ -693,6 +697,7 @@ public final class ShutdownThread extends Thread {
      */
     public static void rebootOrShutdown(final Context context, boolean reboot, String reason) {
         if (reboot) {
+            stopShutdownAnimation();
             Log.i(TAG, "Rebooting, reason: " + reason);
             PowerManagerService.lowLevelReboot(reason);
             Log.e(TAG, "Reboot failed, will attempt shutdown instead");
@@ -713,6 +718,9 @@ public final class ShutdownThread extends Thread {
             } catch (InterruptedException unused) {
             }
         }
+
+        stopShutdownAnimation();
+
         // Shutdown power
         Log.i(TAG, "Performing low-level shutdown...");
         PowerManagerService.lowLevelShutdown(reason);
@@ -802,6 +810,26 @@ public final class ShutdownThread extends Thread {
                 FileUtils.stringToFile(RecoverySystem.UNCRYPT_STATUS_FILE, timeoutMessage);
             } catch (IOException e) {
                 Log.e(TAG, "Failed to write timeout message to uncrypt status", e);
+            }
+        }
+    }
+
+    private static boolean themeShutdownAnimationExists() {
+        return new File("/data/system/theme/shutdownanimation.zip").exists();
+    }
+
+    private static void startShutdownAnimation() {
+        SystemProperties.set("service.bootanim.exit", "0");
+        SystemProperties.set("sys.powerctl", "animate");
+        SystemProperties.set("ctl.start", "bootanim");
+    }
+
+    private static void stopShutdownAnimation() {
+        SystemProperties.set("service.bootanim.exit", "1");
+        while (SystemProperties.get("init.svc.bootanim").equals("running")) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException unused) {
             }
         }
     }
